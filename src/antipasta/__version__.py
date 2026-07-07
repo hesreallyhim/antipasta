8c3868f923b5e5b4041b852eebb3017c90f6bfbd
Really Him <git-dev@hesreallyhim.com>
2026-07-07 18:20:11 -0400

    ci: add release-please automation

--- src/antipasta/__version__.py
+++ src/antipasta/__version__.py
@@ -1,4 +1,4 @@
 """Version information for antipasta."""
 
-__version__ = "1.1.2"
+__version__ = "1.1.2"  # x-release-please-version
 __version_info__ = tuple(int(i) for i in __version__.split("."))
